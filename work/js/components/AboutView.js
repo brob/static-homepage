7cc9bf20d32d61eba22a86647483001bab31d2a4
Bryan Robinson <bryanlrobinson@gmail.com>
2015-05-29 09:04:22 -0500

    Removed Medium link and aside from about

--- work/js/components/AboutView.js
+++ work/js/components/AboutView.js
@@ -6,7 +6,7 @@ var AboutView = React.createClass({
 
         return (
                 <div className="large-10 large-centered columns about">
-                    <p>Hello there! I'm Bryan Robinson, and &mdash; you know what &mdash; I'm a design and development strategist with a strong focus on my end user's goals. You can visit me where I live on the web below, and YES, the icons wiggle. Sue me, css3 keyframes are fun. </p>
+                    <p>Hello there! I'm Bryan Robinson, and I'm a design and development strategist with a strong focus on my end user's goals. You can visit me where I live on the web below, and YES, the icons wiggle. Sue me, css3 keyframes are fun. </p>
                 </div>
 
 
